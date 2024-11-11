586d4a492cf5738bb905b3b645d898b4f94d656a
nickygp <nigarciapa@uide.edu.ec>
2024-11-10 20:34:48 -0500

    Mi primer commit

---
+++ base/clase1.go
@@ -0,0 +1,19 @@
+import "fmt"
+
+package main
+
+func main() {
+	fmt.Println("Hola a todos")
+}
+
+/* 
+todos los archivos deben ir comentados asi:
+@autor: Nicolas Garcia
+@fecha 10/11/2024
+@descripcion: Clase 1 de programacion
+*/
+
+// Librerias a importar y utlizar dentro del archivo
+//Libreria fmt permite imprimir en pantalla
+
+
